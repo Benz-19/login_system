f9b6316b0352d0854241204c4b547bf45240bd5c
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-18 16:55:24 +0300

    update

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+from login_system import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User()
+    username = request.form['username']
+    password = request.form['password']
+    hashed_pass = user.hash_password(password)
+
+    if user.login_user(username.capitalize(), hashed_pass):
+        return f"<h1>Welcome, {username}</h1>"
+    else:
+        return "Login failed"
+
+if __name__ == "__main__":
+    app.run(debug=True)
